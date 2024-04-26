96a6608f1d30e49375ae4ae177f96b40192a8c55
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-04-26 20:05:18 +0530

    initial commit

---
+++ src/components/LandCard.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { mockData } from '../mockData';
+import { PiShareFatBold } from 'react-icons/pi';
+import { MdFavoriteBorder } from 'react-icons/md';
+import { MdFavorite } from 'react-icons/md';
+import { GrNext } from 'react-icons/gr';
+import { GrPrevious } from 'react-icons/gr';
+import { MdVerified } from 'react-icons/md';
+
+const LandCard = ({ data }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const images = data.land_media.map((x) => x.image);
+
+  const handleNext = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevious = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+  return (
+    <>
+      <div className='max-w-lg mx-auto bg-white rounded-lg overflow-hidden shadow-lg'>
+        <div className='relative'>
+          <img
+            className='w-full h-64'
+            src={images[currentImageIndex]}
+            alt='carousel'
+          />
+          <button
+            className='absolute top-1/2 left-1 transform -translate-y-1/2 bg-white bg-opacity-50 text-white rounded-full p-2'
+            onClick={handlePrevious}
+          >
+            <GrPrevious color='black' />
+          </button>
+          <button
+            className='absolute top-1/2 right-1 transform -translate-y-1/2 bg-white bg-opacity-50 text-white rounded-full p-2'
+            onClick={handleNext}
+          >
+            <GrNext color='black' />
+          </button>
+
+          <div className='absolute top-0 right-0 m-2 space-y-2'>
+            <div className='flex flex-row gap-2'>
+              <button className='bg-white rounded-full p-2'>
+                <PiShareFatBold />
+              </button>
+              <button
+                className='bg-white rounded-full p-2'
+                onClick={handleLike}
+              >
+                {isLiked ? <MdFavorite /> : <MdFavoriteBorder />}
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Sample Text */}
+        <div className='flex flex-col'>
+          <div className=' flex flex-row justify-start gap-4 p-2'>
+            <p className='text-black font-bold font-sans'>
+              {data.village_name}, {data.mandal_name}, {data.district_name},{' '}
+              {data.id}
+            </p>
+            {data.is_basic_verified && (
+              <div>
+                <MdVerified color='blue' size={16} />
+              </div>
+            )}
+          </div>
+          <div className='flex flex-row justify-start gap-4 p-2'>
+            <p className='text-black font-semibold font-sans'>
+              {data.total_land_size_in_acres.acres} Acres{' . '}
+              {data.total_land_size_in_acres.guntas} Guntas
+            </p>
+            <p className='text-black font-medium font-sans'>
+              {data.price_per_acre} Lakhs per Acre
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandCard;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import LandCard from '../components/LandCard';
+
+import { FadeLoader } from 'react-spinners';
+
+const Dashboard = () => {
+  const [landData, setLandData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const fetchData = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://prod-be.1acre.in/lands/?ordering=-updated_at&page=${page}&page_size=10`
+      );
+      const data = await response.json();
+
+      setLandData((prevData) => [...prevData, ...data.results]);
+      setPage(page + 1);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+    console.log('page fetching is: ', page);
+  }, []);
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.documentElement.offsetHeight ||
+      isLoading
+    ) {
+      fetchData();
+      console.log('loading is : ', isLoading);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [page]);
+  // console.log('1st result is: ', landData[1]);
+
+  return (
+    <>
+      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+        {landData.map((land) => (
+          <div className='w-full h-96'>
+            <LandCard data={land} />
+          </div>
+        ))}
+      </div>
+      {isLoading && (
+        <>
+          <div className='flex justify-center align-middle'>
+            <FadeLoader
+              color='yellow'
+              height={15}
+              width={5}
+              radius={2}
+              margin={2}
+            />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Dashboard;
